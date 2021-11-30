593f15dfc83e5d751be7ce0747a515e9624760a0
TommY2103 <tovoli.tommaso@gmail.com>
2021-11-30 13:24:22 +0100

    navbar init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { AppBar, CssBaseline, Link, Stack, Toolbar, Typography, useTheme } from "@mui/material";
+import React from "react";
+
+
 function App() {
+  const theme = useTheme();
+
   return (
-    <div>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="fixed" color="transparent" elevation={0}>
+        <Toolbar>
+          <Typography variant="h6" flexGrow={1}>Logo</Typography>
+          <Stack direction="row" gap={theme.spacing(2)}>
+            <Link href="#" color="inherit" underline="none">Home</Link>
+            <Link href="#" color="inherit" underline="none">Cryptos</Link>
+            <Link href="#" color="inherit" underline="none">Dashboard</Link>
+          </Stack>
+          
+        </Toolbar>
+      </AppBar>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createTheme, ThemeProvider, colors } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#121212"
+    },
+    text: {
+      primary: colors.grey[50],
+      secondary: colors.grey[200],
+    }
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
