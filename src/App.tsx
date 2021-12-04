9e85d5faa69d3a3552b918f2e05e3e3ceb529888
TommY2103 <tovoli.tommaso@gmail.com>
2021-12-04 22:10:57 +0100

    navbar with styled items

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,46 @@
-import { AppBar, CssBaseline, Link, Stack, Toolbar, Typography, useTheme } from "@mui/material";
+import {
+  AppBar,
+  CssBaseline,
+  Link,
+  Stack,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { FaUser } from "react-icons/fa";
 import React from "react";
 
+const NavItems = styled(Stack)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  gap: theme.spacing(2),
+}));
 
 function App() {
-  const theme = useTheme();
 
   return (
     <>
       <CssBaseline />
       <AppBar position="fixed" color="transparent" elevation={0}>
-        <Toolbar>
-          <Typography variant="h6" flexGrow={1}>Logo</Typography>
-          <Stack direction="row" gap={theme.spacing(2)}>
-            <Link href="#" color="inherit" underline="none">Home</Link>
-            <Link href="#" color="inherit" underline="none">Cryptos</Link>
-            <Link href="#" color="inherit" underline="none">Dashboard</Link>
-          </Stack>
-          
+        <Toolbar sx={{ justifyContent: "space-around" }}>
+          <Typography variant="h6" color="primary">
+            Logo
+          </Typography>
+          <NavItems>
+            <Link href="#" color="inherit" underline="none">
+              Home
+            </Link>
+            <Link href="#" color="inherit" underline="none">
+              Cryptos
+            </Link>
+            <Link href="#" color="inherit" underline="none">
+              Dashboard
+            </Link>
+            <Link href="#" color="primary" underline="none">
+              <FaUser size="30" />
+            </Link>
+          </NavItems>
         </Toolbar>
       </AppBar>
     </>
--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createTheme, ThemeProvider, colors } from '@mui/material';
+import { ThemeProvider } from '@mui/material';
+import theme from "./theme";
 
-const theme = createTheme({
-  palette: {
-    background: {
-      default: "#121212"
-    },
-    text: {
-      primary: colors.grey[50],
-      secondary: colors.grey[200],
-    }
-  }
-})
+
 
 ReactDOM.render(
   <React.StrictMode>
